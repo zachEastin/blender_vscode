5bf6b6f34657826adbc357159675b6b392eee1d9
zachEastin <zaqypoo@gmail.com>
2024-07-08 09:39:28 -0500

    include extensions directories to addon_directories

--- pythonFiles/include/blender_vscode/environment.py
+++ pythonFiles/include/blender_vscode/environment.py
@@ -3,6 +3,7 @@ import sys
 import addon_utils
 from pathlib import Path
 import platform
+from itertools import chain
 
 python_path = Path(sys.executable)
 blender_path = Path(bpy.app.binary_path)
@@ -17,4 +18,11 @@ else:
 version = bpy.app.version
 scripts_folder = blender_path.parent / f"{version[0]}.{version[1]}" / "scripts"
 user_addon_directory = Path(bpy.utils.user_resource('SCRIPTS', path="addons"))
-addon_directories = tuple(map(Path, addon_utils.paths()))
+if bpy.app.version >= (4, 2, 0):
+    addon_directories = tuple(map(Path, chain(addon_utils.paths(), [
+        ext.directory
+        for ext in bpy.context.preferences.extensions.repos
+        if not ext.use_remote_url
+    ])))
+else:
+    addon_directories = tuple(map(Path, addon_utils.paths()))
